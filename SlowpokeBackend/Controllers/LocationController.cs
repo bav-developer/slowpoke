fe2eb902274bed78db9d06ad839dd4e717092e4a
abiaspalau <alexey.bespalov.2014@gmail.com>
2018-06-25 17:57:58 +0300

    refactoring

--- SlowpokeBackend/Controllers/LocationController.cs
+++ SlowpokeBackend/Controllers/LocationController.cs
@@ -18,34 +18,30 @@ namespace SlowpokeBackend.Controllers
         {
             _locationService = new LocationService();
         }
-        // GET api/values
+
         [HttpGet]
         public IEnumerable<CacheLocation> Get()
         {
             return _locationService.GetAllLocations();
         }
 
-        // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
         [HttpPost]
         public void Post([FromBody]CacheLocation value)
         {
             _locationService.StoreLocation(value);
         }
 
-        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
